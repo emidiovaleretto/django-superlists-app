ea1b10e08aa0968c84a66e0b2b50d6bb03602a9d
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-12 19:34:51 +0100

    Functional tests now checks we can input a to-do item.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.keys import Keys
 
+import unittest
 from time import sleep
 
 
@@ -10,7 +11,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
 
     def tearDown(self) -> None:
-        sleep(5)
+        sleep(2)
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -20,18 +21,35 @@ class NewVisitorTest(unittest.TestCase):
 
         # then she notices the page title and header mention a to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Buy peacock feathers" into the text field
         # Her hobby is tying fly-sishing lures
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and list the item
         # '1: Buy peacock feathers' as an item in her to-do list
+        inputbox.send_keys(Keys.ENTER)
+        sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_element_by_tag_name('tr')
+
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There's still a text box invitting her to add another item.
         # Then she enters "Use peacock feathers to make a fly."
+        self.fail('Finish the test!')
 
         # The page updates again and shows both items on her list
 
