56958fa050a726eba0c857b85180053e4a3f94ba
Emidio Valeretto <emidio.valereto@gmail.com>
2024-10-26 18:48:30 +0100

    chore: check the changes to functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
         sleep(2)
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Liz has heard about a cool new to-do app.
         # She goes to check out its homepage
@@ -39,17 +44,18 @@ class NewVisitorTest(unittest.TestCase):
         # '1: Buy peacock feathers' as an item in her to-do list
         inputbox.send_keys(Keys.ENTER)
         sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There's still a text box invitting her to add another item.
         # Then she enters "Use peacock feathers to make a fly."
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly.')
+        inputbox.send_keys(Keys.ENTER)
+        sleep(1)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly.')
+
         self.fail('Finish the test!')
 
         # The page updates again and shows both items on her list
@@ -63,5 +69,5 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, she goes back to sleep
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 
 def home_page(request):
+    if request.method == 'POST':
+        new_item_text = request.POST.get('item_text', '')
+        context = {
+            'new_item_text': new_item_text
+        }
+        return render(request, 'home.html', context=context)
     return render(request, 'home.html')
