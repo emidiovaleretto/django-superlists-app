e4d3fc29e951cf81b407ac3305f5a44d62cf3b1b
Emidio Valeretto <emidio.valereto@gmail.com>
2024-11-09 18:00:37 +0000

    chore: adjust model so that items are associated with different lists

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 
+class List(models.Model):
+    ...
+
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.text
