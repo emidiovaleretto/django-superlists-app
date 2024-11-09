2c605b8385b090d45b5e149bb976a2a8b008f74b
Emidio Valeretto <emidio.valereto@gmail.com>
2024-11-09 23:11:46 +0000

    chore: new URL + view for adding to existing lists. FT passes =)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -52,7 +52,6 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and list the item
         # '1: Buy peacock feathers' as an item in her to-do list
         inputbox.send_keys(Keys.ENTER)
-        sleep(1)
         self.wait_for_row_in_list_table('1: Buy peacock feathers.')
 
         # There's still a text box invitting her to add another item.
@@ -60,7 +59,6 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly.')
         inputbox.send_keys(Keys.ENTER)
-        sleep(1)
 
         self.wait_for_row_in_list_table('1: Buy peacock feathers.')
         self.wait_for_row_in_list_table(
@@ -73,15 +71,7 @@ class NewVisitorTest(LiveServerTestCase):
         )
         self.wait_for_row_in_list_table('1: Buy peacock feathers.')
 
-        # Liz wonders whether the site will remember her list.
-        # Then she sees that the site has generate an unique URL for her
-        # There is some explanatory text to that effect
-
-        # She visits that URL - her to-do list is still there
-
         # Satisfied, she goes back to sleep
-
-        self.fail('Finish the test!')
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Liz starts a new to-do list
--- lists/urls.py
+++ lists/urls.py
@@ -4,10 +4,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('lists/new', views.new_list, name='new_list'),
     path(
-        'lists/the-only-list-in-the-world/',
+        'lists/<int:list_id>/',
         views.view_list,
         name='view_list'
     ),
-    path('lists/new', views.new_list, name='new_list'),
+    path(
+        'lists/<int:list_id>/add_item',
+        views.add_item,
+        name='add_item'
+    ),
 ]
