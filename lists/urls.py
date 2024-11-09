1c80d2daa78e252c6202da4d1e9615f44bbccabb
Emidio Valeretto <emidio.valereto@gmail.com>
2024-11-09 23:19:20 +0000

    refactor: separate lists/urls.py to make the app more self-contained

--- lists/urls.py
+++ lists/urls.py
@@ -3,15 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/new', views.new_list, name='new_list'),
+    path('new', views.new_list, name='new_list'),
     path(
-        'lists/<int:list_id>/',
+        '<int:list_id>/',
         views.view_list,
         name='view_list'
     ),
     path(
-        'lists/<int:list_id>/add_item',
+        '<int:list_id>/add_item',
         views.add_item,
         name='add_item'
     ),
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
+from lists import views as list_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('lists.urls')),
+    path('', list_views.home_page, name='home'),
+    path('lists/', include('lists.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
